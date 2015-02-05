da29e9e259b99d2936ffa2a80f458a6599e70fca
Nicholas Draper <ndraper2@gmail.com>
2015-02-04 23:30:18 -0800

    added templates base.jinja2 and list.jinja2, which extends base. Then added HTTP fixtures to test_journal.

--- test_journal.py
+++ test_journal.py
@@ -3,6 +3,7 @@ from contextlib import closing
 from pyramid import testing
 import pytest
 import datetime
+import os
 
 from journal import connect_db
 from journal import DB_SCHEMA
@@ -176,3 +177,38 @@ def test_read_entries(req_context):
         assert expected[1] == entry['text']
         for key in 'id', 'created':
             assert key in entry
+
+
+@pytest.fixture(scope='function')
+def app(db):
+    from journal import main
+    from webtest import TestApp
+    os.environ['DATABASE_URL'] = TEST_DSN
+    app = main()
+    return TestApp(app)
+
+
+def test_empty_listing(app):
+    response = app.get('/')
+    assert response.status_code == 200
+    actual = response.body
+    expected = 'No entries here so far'
+    assert expected in actual
+
+
+@pytest.fixture(scope='function')
+def entry(db, request):
+    """provide a single entry in the database"""
+    settings = db
+    now = datetime.datetime.utcnow()
+    expected = ('Test Title', 'Test Text', now)
+    with closing(connect_db(settings)) as db:
+        run_query(db, INSERT_ENTRY, expected, False)
+        db.commit()
+
+    def cleanup():
+        clear_entries(settings)
+
+    request.addfinalizer(cleanup)
+
+    return expected
